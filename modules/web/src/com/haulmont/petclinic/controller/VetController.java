33703b8b8572aa02e97fda1294dc54a3601051bb
Andrey Belyaev <belyaev.andrey@gmail.com>
2018-06-01 19:19:09 +0300

    Updated CUBA version

--- modules/web/src/com/haulmont/petclinic/controller/VetController.java
+++ modules/web/src/com/haulmont/petclinic/controller/VetController.java
@@ -2,6 +2,7 @@ package com.haulmont.petclinic.controller;
 
 import com.haulmont.petclinic.service.EntityJsonXmlExportService;
 import com.haulmont.petclinic.service.VetService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,12 @@ public class VetController {
     @Inject
     private EntityJsonXmlExportService importExportService;
 
-    @GetMapping(value = { "/vets.json" }, produces = "application/json")
+    @GetMapping(value = { "/vets.json" }, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public @ResponseBody String showJsonVetList() {
         return importExportService.exportEntitiesToJSON(vetService.findAll());
     }
 
-    @GetMapping(value = { "/vets.xml" }, produces = "application/xml")
+    @GetMapping(value = { "/vets.xml" }, produces = MediaType.APPLICATION_XML_VALUE)
     public @ResponseBody String showXmlVetList() {
         return importExportService.exportEntitiesToXML(vetService.findAll(), "vets");
     }
