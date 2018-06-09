1e7dc9a5062c4726a60bc4d63e647fb9a137147c
Andrey Belyaev <belyaev.andrey@gmail.com>
2018-06-09 14:45:40 +0300

    Further main window customization

--- modules/web/src/com/haulmont/petclinic/welcome/MainWindow.java
+++ modules/web/src/com/haulmont/petclinic/welcome/MainWindow.java
@@ -1,6 +1,24 @@
 package com.haulmont.petclinic.welcome;
 
+import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.web.app.mainwindow.AppMainWindow;
 
+import java.util.Map;
+
 public class MainWindow extends AppMainWindow {
+
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        initPoweredByLink();
+    }
+
+    private void initPoweredByLink() {
+        Component poweredByLink = getComponent("poweredByLink");
+        if (poweredByLink != null) {
+            poweredByLink.setVisible(webConfig.getLoginDialogPoweredByLinkVisible());
+        }
+    }
+
 }
